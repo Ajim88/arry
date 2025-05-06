9204c6946b43dc3ecf96c1711285824eb25b7d3f
ajim khan <ajkhanbd24@gmail.com>
2025-05-06 23:22:01 +0600

    array

---
+++ arry.js
@@ -0,0 +1,8 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 442;
+// const num4 = 442;
+/*arry*/ 
+const numbers = [12, 22, 54, 65];
+const friends = ['fidu', 'rhaman', 'ismail', 'sojib', 'Hridoy'];
+const passed = [true, false, true, false];
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers = [1, 3, 5, 11, 22, 44, 55, 66];
+console.log(numbers.length);
